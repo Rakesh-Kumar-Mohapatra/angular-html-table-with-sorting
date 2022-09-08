df73a7d700042365e6bc5055152c6bf09e926c67
rkm1997 <71068044+rkm1997@users.noreply.github.com>
2022-09-08 12:54:00 +0530

    4 files modified, 1 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component, VERSION } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { CookiesService } from './services/cookies.service';
 
 @Component({
   selector: 'my-app',
@@ -6,5 +8,47 @@ import { Component, VERSION } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+  columnDefs: any[] = [];
+  rowData: any[] = [];
+  users: any;
+  cookiesData: Subscription;
+  sortDir = 1;//1= 'ASE' -1= DSC
+
+
+  constructor(private api: CookiesService) {}
+
+  ngOnInit(): void {
+    this.cookiesData = this.api.get().subscribe((res) => {
+      this.users = res;
+      this.users = this.users.cookies;
+      console.log('data response', this.users);
+    });
+  }
+  onSortClick(event) {
+    let target = event.currentTarget,
+      classList = target.classList;
+
+    if (classList.contains('fa-chevron-up')) {
+      classList.remove('fa-chevron-up');
+      classList.add('fa-chevron-down');
+      this.sortDir=-1;
+    } else {
+      classList.add('fa-chevron-up');
+      classList.remove('fa-chevron-down');
+      this.sortDir=1;
+    }
+    this.sortArr('name');
+  }
+
+  sortArr(colName:any){
+    this.users.sort((a,b)=>{
+      a= a[colName].toLowerCase();
+      b= b[colName].toLowerCase();
+      return a.localeCompare(b) * this.sortDir;
+    });
+  }
+
+  ngOnDestory(){
+    this.cookiesData.unsubscribe();
+  }
 }
