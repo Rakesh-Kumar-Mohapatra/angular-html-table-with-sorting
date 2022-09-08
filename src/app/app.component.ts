639a34b62aa4e37e5ff02184bb302ac2a643967e
rkm1997 <71068044+rkm1997@users.noreply.github.com>
2022-09-08 13:27:40 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,20 +5,16 @@ import { CookiesService } from './services/cookies.service';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit, OnDestroy {
   columnDefs: any[] = [];
   rowData: any[] = [];
   users: any;
   cookiesData: Subscription;
-  sortDir = 1;//1= 'ASE' -1= DSC
-
+  sortDir = 1; //1= 'ASE' -1= DSC
 
   constructor(private api: CookiesService) {}
-ngOnDestroy(): void {
-throw new Error('Method not implemented.');
-}
 
   ngOnInit(): void {
     this.cookiesData = this.api.get().subscribe((res) => {
@@ -31,36 +27,36 @@ throw new Error('Method not implemented.');
     let target = event.currentTarget;
     let classList = target.classList;
     let t = event;
-    let id = target.innerHTML.split("<i")[0];
-    console.log("sortclick", target.innerHTML.split("<i")[0]);
+    let id = target.innerHTML.split('<i')[0];
+    console.log('sortclick', target.innerHTML.split('<i')[0]);
     if (classList.contains('fa fa-chevron-up')) {
       classList.remove('fa-chevron-up');
       classList.add('fa-chevron-down');
-      this.sortDir=-1;
+      this.sortDir = -1;
     } else {
       classList.add('fa-chevron-up');
       classList.remove('fa-chevron-down');
-      this.sortDir=1;
+      this.sortDir = 1;
     }
-    if(id == "Product Name") {
-      console.log("sortclick", target.innerHTML.split("<i")[0]);
+    if (id == 'Product Name') {
+      console.log('sortclick', target.innerHTML.split('<i')[0]);
       this.sortArr('name');
-    } else if(id == "Price") {
-      console.log("sortclick", target.innerHTML.split("<i")[0]);
+    } else if (id == 'Price') {
+      console.log('sortclick', target.innerHTML.split('<i')[0]);
       this.sortArr('price');
-    } else if(id == "Category") {
-      console.log("sortclick", target.innerHTML.split("<i")[0]);
+    } else if (id == 'Category') {
+      console.log('sortclick', target.innerHTML.split('<i')[0]);
       this.sortArr('category');
     }
   }
-  sortArr(colName:any){
-    this.users.sort((a,b)=>{
-      a= a[colName].toLowerCase();
-      b= b[colName].toLowerCase();
+  sortArr(colName: any) {
+    this.users.sort((a, b) => {
+      a = a[colName].toLowerCase();
+      b = b[colName].toLowerCase();
       return a.localeCompare(b) * this.sortDir;
     });
   }
-  ngOnDestory(){
+  ngOnDestroy(): void {
     this.cookiesData.unsubscribe();
   }
 }
